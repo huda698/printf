c7513a7386150fd4b687e061affe0d072a5744e4
huda <hodamohammed076@gmail.com>
2023-07-24 05:33:43 -0700

    parameters

---
+++ params.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * init_params - clear fields and reset buf.
+ *
+ * @params: struct parameters.
+ *
+ * @az: argument pointer.
+ */
+
+void init_params(params_t *params, va_list az)
+{
+	params->unsign = 0;
+	params->plus = 0;
+	params->space = 0;
+	params->hashtag = 0;
+	params->zero = 0;
+	params->minus = 0;
+	params->width = 0;
+	params->precision = UNIT_MAX;
+	params->h_modif = 0;
+	params->l_modif = 0;
+	(void)az;
+}

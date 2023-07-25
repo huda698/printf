c973a796584b59522077b796d8ecce4a577590b2
5599eman <emanmohamedalmahdy@gmail.com>
2023-07-25 13:45:51 -0700

    convert_number.c

--- convert_number.c
+++ convert_number.c
@@ -13,14 +13,14 @@ int print_hex(va_list az, params_t *params)
 	int c = 0;
 	char *str;
 
-	if (params->l_modifier)
+	if (params->l_modif)
 		l = (unsigned long)va_arg(az, unsigned long);
-	else if (params->h_modifier)
+	else if (params->h_modif)
 		l = (unsigned short int)va_arg(az, unsigned int);
 	else
 		l = (unsigned int)va_arg(az, unsigned int);
 	str = convert(l, 16, CONVERT_UNSIGNED | CONVERT_LOWERCASE, params);
-	if (params->hashtag_flag && l)
+	if (params->hashtag && l)
 	{
 		*--str = 'x';
 		*-str = '0';
@@ -42,14 +42,14 @@ int print_HEX(va_list az, params_t *params)
 	int c = 0;
 	char *str;
 
-	if (params->l_modifier)
+	if (params->l_modif)
 		l = (unsigned long)va_arg(az, unsigned long);
-	else if (params->h_modifier)
+	else if (params->h_modif)
 		l = (unsigned short int)va_arg(az, unsigned int);
 	else
 		l = (unsigned int)va_arg(az, unsigned int);
 	str = convert(l, 16, CONVERT_UNSIGNED, params);
-	if (params->hashtag_flag && l)
+	if (params->hashtag && l)
 	{
 		*--str = 'X';
 		*--str = '0';
@@ -71,7 +71,7 @@ int print_binary(va_list az, params_t *params)
 	char *str = convert(n, 2, CONVERT_UNSIGNED, params);
 	int c = 0;
 
-	if (params->hashtag_flag && n)
+	if (params->hashtag && n)
 		*--str = '0';
 	params->unsign = 1;
 	return (c += print_number(str, params));
@@ -90,15 +90,15 @@ int print_octal(va_list az, params_t *params)
 	char *str;
 	int c = 0;
 
-	if (params->l_modifier)
+	if (params->l_modif)
 		l = (unsigned long)va_arg(az, unsigned long);
-	else if (params->h_modifier)
+	else if (params->h_modif)
 		l = (unsigned short int)va_arg(az, unsigned int);
 	else
 		l = (unsigned int)va_arg(az, unsigned int);
 	str = convert(l, 8, CONVERT_UNSIGNED, params);
 
-	if (params->hashtag_flag && l)
+	if (params->hashtag && l)
 		*--str = '0';
 	params->unsign = 1;
 	return (c += print_numbers(str, params));
